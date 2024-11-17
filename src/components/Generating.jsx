aaef6f09fe51f1138d10f6ef8871a9b9e438c61c
Faiz <220701167@rajalakshmi.edu.in>
2024-11-17 18:41:15 +0530

    initial commit

---
+++ src/components/Generating.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { loading } from '../assets'
+
+const Generating = ({ className }) => {
+  return (
+    <div className={`flex items-center h-[3.5rem] text-n-1 px-6 bg-n-8/80 backdrop-blur-sm rounded-full ${className || ''} text-base `}>
+        <img src={loading} alt="loading" className='w-5 h-5 mr-4' />
+        AI is generating...
+    </div>
+  )
+}
+
+export default Generating
